8a8092fefcb4dfa678559f6c4faa6ea65348a880
kstephens0331 <Kyle_stephens31@icloud.com>
2025-08-05 19:55:16 -0500

    update index.css and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: "#1D4ED8", // Replace with your preferred color
+        primary: "#1D4ED8", // your preferred theme color
         accent: "#f97316",
       },
     },
   },
   plugins: [],
-}
+};
